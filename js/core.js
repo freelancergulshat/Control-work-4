bf3f16f096afb743d3d4b19bd55751321018284f
Gulshat <arkytbekovagulshat@gmail.com>
2019-02-24 22:49:03 +0600

    Gallery block is completed

--- js/core.js
+++ js/core.js
@@ -1,18 +1,104 @@
-$(document).ready(function(){
+$(document).ready(function(){/*
+	$('#registr-form').validate({
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			digits: {
+				number: true,
+				required: true,
+				minlength: 8,
+				maxlength: 40
+			}
+		},
+		messages: {
+			name:{
+				required: "Введите ваше имя"
+			},
+			email: {
+				required: "Введите ваш email адрес",
+				email: "заполните email правильно"
+			},
+			digits: {
+				required: "введите ваш номер телефона",
+				number: "введите ваш номер телефона цифрами",
+				minlength: "Минималное количество символов должно быть 8",
+				maxlength: "Минималное количество символов должно быть 20"
+			},
+		}
+	});	
 
-//	$('.tabs').on('click', 'span:not(.active)', function(){
 
-//		var ind = $(this).index();
-//		$(this).addClass('active').siblings().removeClass('active');
-//		$(this).closest('.tabs-block').find('.tab-content').removeClass('active').eq(ind).addClass('active');
+	var open_modal = $('.open_modal');
+	var overlay = $('.overlay');
+	var modal = $('.modal');
+	var close = $('.close, .overlay');
+	$('#contact-form').validate({
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			digits: {
+				number: true,
+				required: true,
+				minlength: 8,
+				maxlength: 40
+			}
+		},
+		messages: {
+			name:{
+				required: "Введите ваше имя"
+			},
+			email: {
+				required: "Введите ваш email адрес",
+				email: "заполните email правильно"
+			},
+			digits: {
+				required: "введите ваш номер телефона",
+				number: "введите ваш номер телефона цифрами",
+				minlength: "Минималное количество символов должно быть 8",
+				maxlength: "Минималное количество символов должно быть 20"
+			},
+		}
+	});
 
-//	})
-	
+	open_modal.click(function(event){
+		event.preventDefault;
+
+		var current_modal = $(this).attr('href');
+		overlay.fadeIn(700, function(){
+			$(current_modal).css('display', 'block').animate({top: '285px'}, 400);
+		});
+	});
+
+	close.click(function(){
+
+		modal.fadeOut(700, function(){
+			modal.css('top', '20%');
+		});
+		overlay.fadeOut(700);
+	});
+
+	$('.nav').on('click', 'a', function(event){
+		event.proventDefault;
+		var id = $(this).attr('href');
+		var top = $(id).offset().top;
+
+		$('body, html').animate({scrollTop: top},2000)
+	});
 
 	$(window).scroll(function(){
 
 		var top_scroll = $(document).scrollTop();
-		var top_fix = $('#block').offset().top;
+		var top_fix = $('#gallery').offset().top - 5;
 
 		if (top_scroll > top_fix) {
 			$('header').css('top', '-80px').addClass('fix').animate({top: '0'}, 1000);
@@ -20,6 +106,6 @@ $(document).ready(function(){
 			$('header').removeClass('fix');
 		};
 
-	});
+	});*/
 
 });
